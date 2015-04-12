addb1a4891d588c47cfbd07a1ebd2bbf2c6ed95e
Mike <michael.lenz@gmail.com>
2015-04-12 13:42:36 -0500

    Updated wsgi file

--- coffeed/wsgi.py
+++ coffeed/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffeed.settings")
 
+from dj_static import Cling
+
 application = get_wsgi_application()
