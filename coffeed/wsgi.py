3ef9ab29468f095e6c1669649be53e1ddda01376
Mike <michael.lenz@gmail.com>
2015-04-12 13:46:20 -0500

    updated wsgi

--- coffeed/wsgi.py
+++ coffeed/wsgi.py
@@ -15,4 +15,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffeed.settings")
 
 from dj_static import Cling
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
